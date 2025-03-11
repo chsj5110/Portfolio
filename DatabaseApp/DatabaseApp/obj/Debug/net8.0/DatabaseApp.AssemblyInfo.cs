e03018d00183c0164cf7ef90ac6e37f75167ba6e
Soojin <chsj5110@gmail.com>
2025-03-11 17:22:19 +0900

    update Blazor

--- DatabaseApp/DatabaseApp/obj/Debug/net8.0/DatabaseApp.AssemblyInfo.cs
+++ DatabaseApp/DatabaseApp/obj/Debug/net8.0/DatabaseApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc0ce6e8d9e13684eff6dd41e106bf4777cdd00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7926265fe7d4666b23a78b6842969176884eee28")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
