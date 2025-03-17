4dd7c203aa02e70befb62599470b87f99537bc39
Soojin <chsj5110@gmail.com>
2025-03-17 22:06:58 +0900

    Middleware

---
+++ UDP_Server_Middleware/UDP_Server_Middleware/DBConnector.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Messaging;
+using UDP_Server_Middleware;
+
+namespace UDP_server
+{
+    class DBConnector : Program
+    {
+        public static SqlConnection _sqlConnection;
+
+
+        // DB 연결 열기
+        public static void MssqlOpen()
+        {
+            try
+            {
+                if (_sqlConnection == null)
+                {
+                    _sqlConnection = new SqlConnection
+                    {
+                        ConnectionString = string.Format($"Data Source={DataSource}; " +
+                                                         $"Initial Catalog={InitialCatalog};" +
+                                                         $"Persist Security Info=True;" +
+                                                         $"User ID={UserID};" +
+                                                         $"Password={Password}")
+                    };
+                }
+
+                if (_sqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                    _sqlConnection.Open();
+                    //Console.WriteLine("DB 연결 성공");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"MssqlOpen : DB 연결 실패 / {ex.Message}");
+            }
+        }
+
+        // DB 연결 닫기
+        public static void MssqlClose()
+        {
+            try
+            {
+                if (_sqlConnection != null && _sqlConnection.State == System.Data.ConnectionState.Open)
+                {
+                    _sqlConnection.Close();
+                    _sqlConnection.Dispose();
+                    _sqlConnection = null;
+                    //Console.WriteLine("DB 접속 종료 성공");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"MssqlClose : DB 접속 종료 실패 /  {ex.Message}");
+            }
+        }
+
+       
+        #region insert DB
+
+        static public void headerDisplay(string[] dataArr)
+        {
+
+            string insertQuery = string.Empty;
+            try
+            {
+
+                if (dataArr.Length > 0 && dataArr.Length == 8)
+                {
+                    //insertQuery = 
+
+                }
+                // [GID] 포함
+                else if (dataArr.Length > 0 && dataArr.Length == 9)
+                {
+                    //insertQuery = 
+
+                }
+                // result가 1이면 insert 성공
+                int result = ExecuteNonQuery(insertQuery);
+                if (result != 1)
+                {
+                    Console.WriteLine("display 데이터 insert 실패");
+                    insertQuery = "INSERT INTO exception_log (location, msg) VALUES('headerDisplay', 'display 데이터 insert 실패');";
+                    ExecuteNonQuery(insertQuery);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("display 데이터 insert 실패");
+                insertQuery = "INSERT INTO exception_log (location, msg) VALUES('headerDisplay', 'display 데이터 insert 실패');";
+                ExecuteNonQuery(insertQuery);
+            }
+
+
+        }
+
+        static public void headerStatus2(string[] dataArr)
+        {
+            string insertQuery = string.Empty;
+            try
+            {
+                if (dataArr.Length > 0)
+                {
+                    //insertQuery = 
+                }
+
+
+                // result가 1이면 insert 성공
+                int result = ExecuteNonQuery(insertQuery);
+                if (result != 1)
+                {
+                    Console.WriteLine("status2 데이터 insert 실패");
+                    insertQuery = "INSERT INTO exception_log (location, msg) VALUES('headerStatus2', 'status2 데이터 insert 실패');";
+                    ExecuteNonQuery(insertQuery);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("status2 데이터 insert 실패");
+                insertQuery = "INSERT INTO exception_log (location, msg) VALUES('headerStatus2', 'status2 데이터 insert 실패');";
+                ExecuteNonQuery(insertQuery);
+
+            }
+
+
+
+        }
+
+        static public void headerStatus3(string[] dataArr)
+        {
+            string insertQuery = string.Empty;
+            try
+            {
+                if (dataArr.Length > 0)
+                {
+                    if (dataArr[1].Contains("ANC"))
+                    {
+                        //insertQuery = 
+                    }
+                    else if (dataArr[1].Contains("TAG"))
+                    {
+                        //insertQuery = 
+                    }
+                }
+
+
+                // result가 1이면 insert 성공
+                int result = ExecuteNonQuery(insertQuery);
+                if (result != 1)
+                {
+                    Console.WriteLine("status3 데이터 insert 실패");
+                    insertQuery = "INSERT INTO exception_log (location, msg) VALUES('headerStatus3', 'status3 데이터 insert 실패');";
+                    ExecuteNonQuery(insertQuery);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("status3 데이터 insert 실패");
+                insertQuery = "INSERT INTO exception_log (location, msg) VALUES('headerStatus3', 'status3 데이터 insert 실패');";
+                ExecuteNonQuery(insertQuery);
+            }
+
+
+
+        }
+
+
+        #endregion
+
+
+
+        // 테이블이 존재하는지 확인하고 없으면 생성
+        public static void IsDbExists(string tableName)
+        {
+            try
+            {
+                // 테이블 존재 여부 확인 쿼리
+                string sqlQuery = $"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_CATALOG='' AND TABLE_NAME='{tableName}'";
+                int result = ExecuteScalarQuery(sqlQuery); // COUNT(*) 결과 가져오기
+
+                if (result == 0) // 테이블이 없으면 생성
+                {
+                    Console.WriteLine("테이블이 존재하지 않습니다. 테이블을 생성합니다.");
+                    string createTableQuery = GetCreateTableQuery(tableName);
+
+                    if (!string.IsNullOrEmpty(createTableQuery))
+                    {
+                        int resultCreate = ExecuteNonQuery(createTableQuery); // 테이블 생성 및 더미 데이터 삽입
+                        if (resultCreate > 0)
+                        {
+                            Console.WriteLine($"테이블 생성 성공 : {tableName}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"테이블 생성에 실패하였습니다. : {tableName}");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"테이블이 이미 존재합니다: {tableName}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"오류 발생: {ex.Message}");
+            }
+        }
+
+        public static string ExecuteReaderQuery(string query)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                if (_sqlConnection == null || _sqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                    MssqlOpen();
+                }
+
+                using (SqlCommand command = new SqlCommand(query, _sqlConnection))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if (reader != null)
+                        {
+                            //result = 
+                        }
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ExecuteReaderQuery 오류: {ex.Message}");
+            }
+
+            return result;
+        }
+
+        // 쿼리 실행 후 단일 값 반환
+        public static int ExecuteScalarQuery(string query)
+        {
+            int result = 0;
+            try
+            {
+                if (_sqlConnection == null || _sqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                    MssqlOpen();
+                }
+
+                using (SqlCommand command = new SqlCommand(query, _sqlConnection))
+                {
+                    object scalarResult = command.ExecuteScalar();
+                    if (scalarResult != null)
+                    {
+                        result = Convert.ToInt32(scalarResult);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ExecuteScalarQuery 오류: {ex.Message}");
+            }
+            return result;
+        }
+
+        // 쿼리 실행 (CREATE/INSERT 등)
+        public static int ExecuteNonQuery(string query)
+        {
+            int result = 0;
+            try
+            {
+                if (_sqlConnection == null || _sqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                    MssqlOpen();
+                }
+
+                using (SqlCommand command = new SqlCommand(query, _sqlConnection))
+                {
+                    result = command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ExecuteNonQuery 오류: {ex.Message}");
+            }
+            return result;
+        }
+
+        // 테이블 생성 쿼리 반환
+        private static string GetCreateTableQuery(string tableName)
+        {
+
+
+            switch (tableName)
+            {
+                //
+                default:
+                    return null;
+            }
+
+
+        }
+
+
+
+
+    }
+
+}
+
+
---
+++ UDP_Server_Middleware/UDP_Server_Middleware/Program.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using UDP_server;
+
+namespace UDP_Server_Middleware
+{
+    class Program
+    {
+        static string receivedMessage = string.Empty;
+        static string receivedMessageFromRTLS = string.Empty;
+        public static int Port = 0;
+        public static string DataSource = string.Empty;
+        public static string InitialCatalog = string.Empty;
+        public static string UserID = string.Empty;
+        public static string Password = string.Empty;
+        static void Main(string[] args)
+        {
+
+            // port 번호 받기
+            GetPort();
+            // DB 정보 받기
+            GetDBInfo();
+
+            try//예외 처리
+            {
+
+                checkDBTables();
+
+                //int port = 53000; // 서버에서 사용할 포트 번호
+                UdpClient udpServer = new UdpClient(Port);
+
+                Console.WriteLine($"{Port} 포트에서 대기 중...");
+
+                while (true)
+                {
+                    DataSet dataSet = new DataSet();
+
+                    // 클라이언트로부터 데이터 수신 및 연결 확인
+                    if (IsClientConnected(udpServer, TimeSpan.FromSeconds(5)))
+                    {
+                        // 헤더구분
+                        (string header, string data) = splitHeader(receivedMessage);
+                        string[] dataArr = data.Split(',');
+                        switch (header)
+                        {
+                            case "display":
+                                DBConnector.headerDisplay(dataArr);
+                                
+                                udpClient(data, header);    // 데이터 다시 송신
+                                break;
+                            case "status2":
+                                DBConnector.headerStatus2(dataArr);
+                                break;
+                            case "status3":
+                                DBConnector.headerStatus3(dataArr);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{DateTime.Now} : 클라이언트와 연결이 끊어졌습니다. 다시 연결 시도 중...");
+                        continue; // 다시 대기
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("실패");
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        // 포트번호 입력받기
+        static void GetPort()
+        {
+            while (Port == 0)
+            {
+                Console.WriteLine("UDP 서버에서 사용할 포트 번호를 입력하세요 : ");
+                Int32.TryParse(Console.ReadLine(), out Port);
+
+                // 입력하지 않았거나 숫자 입력이 아닐때
+                if (Port == 0)
+                {
+                    Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요.");
+                }
+            }
+        }
+        static void GetDBInfo()
+        {
+            // DB 정보 입력받기
+            Console.WriteLine("DB 서버 IP를 입력하세요 : ");
+            DataSource = Console.ReadLine();
+            Console.WriteLine("연결할 DB 이름을 입력하세요 : ");
+            InitialCatalog = Console.ReadLine();
+            Console.WriteLine("DB 서버 계정 ID를 입력하세요 : ");
+            UserID = Console.ReadLine();
+            Console.WriteLine("DB 서버 계정 비밀번호를 입력하세요 : ");
+            Password = Console.ReadLine();
+        }
+
+        // udpServer : RTLS로부터 DB 요청 받을때
+        static public void udpServerForRTLS(UdpClient udpServer)
+        {
+            while (true)
+            {
+                try
+                {
+                    // 클라이언트로부터 데이터 수신
+                    IPEndPoint clientEndpoint = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] receivedBytes = udpServer.Receive(ref clientEndpoint);
+                    string receivedMessage = Encoding.UTF8.GetString(receivedBytes);
+
+                    // 클라이언트 정보 출력
+                    Console.WriteLine($"[{clientEndpoint.Address}:{clientEndpoint.Port}]로부터 메시지 수신: {receivedMessage}");
+
+                    // 클라이언트에 응답 전송 (옵션)
+                    string responseMessage = "메시지 수신 확인!";
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+                    udpServer.Send(responseBytes, responseBytes.Length, clientEndpoint);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"오류 발생: {ex.Message}");
+                }
+            }
+        }
+
+        // udpClient
+        static public void udpClient(string data, string header)
+        {
+            UdpClient udpClient = new UdpClient();
+            string serverIP = "127.0.0.1"; // 서버 주소
+            int serverPort = 12345;        // 서버 포트
+
+            string message = header + ',' + data;
+
+            //Console.WriteLine($"RTLS 프로그램으로 send : {message}");
+
+            // 데이터 전송
+            byte[] sendData = Encoding.UTF8.GetBytes(message);
+
+            try
+            {
+                udpClient.Send(sendData, sendData.Length, serverIP, serverPort);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        // 테이블 유무 확인 및 생성하고 테스트 데이터 삽입
+        static void checkDBTables()
+        {
+            DBConnector.IsDbExists("display");
+            DBConnector.IsDbExists("status2");
+            DBConnector.IsDbExists("status3");
+
+            DBConnector.IsDbExists("exception_log");
+        }
+
+        // 클라이언트 메시지 수신이 계속 되고 있는지 확인
+        static bool IsClientConnected(UdpClient udpServer, TimeSpan timeout)
+        {
+            try
+            {
+                IPEndPoint clientEndpoint = new IPEndPoint(IPAddress.Any, 0);
+                // 타임아웃 설정
+                Task<byte[]> receiveTask = Task.Run(() =>
+                {
+                    return udpServer.Receive(ref clientEndpoint);
+                });
+
+                if (receiveTask.Wait(timeout))
+                {
+                    byte[] receivedBytes = receiveTask.Result;
+                    receivedMessage = Encoding.UTF8.GetString(receivedBytes);
+                    Console.WriteLine($"{clientEndpoint.Address} 클라이언트로부터 메시지 수신: {receivedMessage}");
+                    // RTLS
+                    if (receivedMessage == "RTLS : DB Info Request")
+                    {
+                        // 클라이언트에 응답 전송 (옵션)
+                        string responseMessage = $"DBInfo,{DataSource},{InitialCatalog},{UserID},{Password}";
+                        byte[] responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+                        udpServer.Send(responseBytes, responseBytes.Length, clientEndpoint);
+                        Console.WriteLine($"{clientEndpoint.Address} 클라이언트로 응답 전송: {responseMessage}");
+                    }
+
+
+                    return true; // 데이터 수신 성공
+                }
+                else
+                {
+                    return false; // 타임아웃 발생
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"오류 발생: {ex.Message}");
+                return false;
+            }
+        }
+
+
+        // 수신한 메시지 헤더 구분하기
+        // header와 나머지 패킷
+        static public (string, string) splitHeader(string data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                throw new ArgumentException("입력 문자열이 비어있거나 null입니다.");
+            }
+
+            string[] parts = data.Split(':');
+            if (parts.Length < 2)
+            {
+                throw new ArgumentException("입력 문자열에 두 번째 항목이 존재하지 않습니다.");
+            }
+
+            int startIndex = 2; 
+            string result = string.Join(":", parts.Skip(startIndex));
+
+            return (parts[1], result.Trim());
+        }
+
+    }
+}
+
+
